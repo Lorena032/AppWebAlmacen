fea9cd6aab533fa343842a5f78b3025eeaf59b72
hhmal <hhmal@DESKTOP-E5ARD78>
2023-04-19 18:22:40 -0600

    Clase Usuario en la carpeta entidades

---
+++ AppWebAlmacen/Entidades/ClUsuarioEn.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppWebAlmacen.Entidades
+{
+    public class ClUsuarioEn
+    {
+        public int idUsuario { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public string clave { get; set; }
+        public int idRol { get; set; }
+    }
+}
