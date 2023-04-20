235aa9115f25bba611dfeff899089db128073c23
sandra567890 <spgonzalez385@misena.edu.co>
2023-04-20 06:35:37 -0500

    Diseño Master Page

--- AppWebAlmacen/Site1.Master.cs
+++ AppWebAlmacen/Site1.Master.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace AppWebAlmacen.Vista
+namespace AppWebAlmacen
 {
     public partial class Site1 : System.Web.UI.MasterPage
     {
